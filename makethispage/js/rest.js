9813c0bcbfd9cb1ecc8e9019fae6ee3b2c5ff6a3
MatiasHernanSerrato <matiashernanserrato@gmail.com>
2017-01-25 12:43:50 -0300

    25/01/2017 12:43 p.m.

--- makethispage/js/rest.js
+++ makethispage/js/rest.js
@@ -1,3 +1,3 @@
 //comment: I wanna know the way to learn quickly this language.
 var x = 12;
-alert("THIS IS AN ALERT BOX TO KNOW IF THIS IS WORKING");
+alert("THIS IS AN ALERT BOX TO KNOW IF THIS IS WORKING. NEW CHANGES");
