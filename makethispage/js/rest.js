7fbf1f55b8dc809e693ec656c389cf8624213931
MatiasHernanSerrato <matiashernanserrato@gmail.com>
2017-01-29 16:46:22 -0300

    ultimate 29/01/2017

--- makethispage/js/rest.js
+++ makethispage/js/rest.js
@@ -1,10 +1,16 @@
 //comment: I wanna know the way to learn quickly this language.
 var x = 12;
-/*whoAreYou();
+
+whoAreYou();
 function whoAreYou(){
-  var user = prompt("Please enter your name here:");
-  aler(user);
-}*/
+  var user = prompt("Please enter your name here (Only 5 letters):");
+  if (user.length >5){
+    alert("ERROR!");
+  }else{
+  alert(user);
+  }
+}
+
 function leave(){
   var question = confirm("Do you wanna leave this page?");
   if(question==true){
